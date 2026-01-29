7e3485779423c8142f5c4a87224f1d89729aaa44
yo_z <johnzeru73@gmail.com>
2026-01-29 17:28:51 +0300

    john

--- database/db.py
+++ database/db.py
@@ -1,10 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy()
-ma = Marshmallow()
 migrate = Migrate()
+ma = Marshmallow()
 
 def init_db(app):
     db.init_app(app)
